4236704ff41afec517e151ee4f43c15541a37880
alexist98 <alexistoigo@gmail.com>
2020-04-16 13:35:33 -0300

    criada função de login.

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,8 +1,9 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:scoped_model/scoped_model.dart';
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class UserModel extends Model {
   FirebaseAuth _auth = FirebaseAuth.instance;
@@ -11,6 +12,14 @@ class UserModel extends Model {
   Map<String, dynamic> userData = Map();
 
   bool isLoading = false;
+
+
+  @override
+  void addListener(VoidCallback listener) {
+    super.addListener(listener);
+
+    _loadCurrentUser();
+  }
 
   void signUp({@required Map<String, dynamic> userData,
     @required String pass,
@@ -37,13 +46,25 @@ class UserModel extends Model {
     });
   }
 
-  void signIn() async {
+  void signIn(
+      {@required String email, @required String pass, @required VoidCallback onSuccess, @required VoidCallback onFail}) async {
     isLoading = true;
     notifyListeners();
 
-    await Future.delayed(Duration(seconds: 3));
-    isLoading = false;
-    notifyListeners();
+    _auth.signInWithEmailAndPassword(email: email, password: pass).then(
+            (user) async {
+          firebaseUser = user;
+
+          await _loadCurrentUser();
+
+          onSuccess();
+          isLoading = false;
+          notifyListeners();
+        }).catchError((e) {
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
   }
 
   void signOut() async{
@@ -56,6 +77,10 @@ class UserModel extends Model {
 
   void recoverPass() {}
 
+  bool isLoggedIn() {
+    return firebaseUser != null;
+  }
+
   Future<Null> _saveUserData(Map<String, dynamic> userData) async {
     this.userData = userData;
     await Firestore.instance
@@ -64,8 +89,19 @@ class UserModel extends Model {
         .setData(userData);
   }
 
-  bool isLoggedIn() {
-    return firebaseUser != null;
+  Future<Null> _loadCurrentUser() async {
+    if (firebaseUser == null)
+      firebaseUser = await _auth.currentUser();
+    if (firebaseUser != null) {
+      if (userData["name"] == null) {
+        DocumentSnapshot docUser =
+        await Firestore.instance.collection("users")
+            .document(firebaseUser.uid)
+            .get();
+        userData = docUser.data;
+      }
+    }
+    notifyListeners();
   }
 
 }
