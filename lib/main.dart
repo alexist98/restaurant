a2ccdb10fb47b48be1d087064ca6686ac5c1c9ef
alexist98 <alexistoigo@gmail.com>
2020-04-16 14:39:35 -0300

    criadas e iniciadas metodos de inserir e remover itens do carrinho.

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,27 @@ import 'package:restaurante/models/user_model.dart';
 import 'package:restaurante/screens/home_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 
+import 'models/cart_model.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
-        model: UserModel(),
-        child: MaterialApp(
-            title: 'Erechim Lanches',
-            theme: ThemeData(
-                primarySwatch: Colors.blue,
-                primaryColor: Color.fromARGB(255, 4, 125, 141)),
-            debugShowCheckedModeBanner: false,
-            home: HomeScreen()));
+      model: UserModel(),
+      child: ScopedModelDescendant(builder: (context, child, model) {
+        return ScopedModel<CartModel>(
+          model: CartModel(model),
+          child: MaterialApp(
+              title: 'Erechim Lanches',
+              theme: ThemeData(
+                  primarySwatch: Colors.blue,
+                  primaryColor: Color.fromARGB(255, 4, 125, 141)),
+              debugShowCheckedModeBanner: false,
+              home: HomeScreen()),
+        );
+      }),
+    );
   }
 }
