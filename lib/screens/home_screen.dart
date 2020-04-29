d6f083a33cbb39911692f7aa9ce808660a92570c
alexist98 <alexistoigo@gmail.com>
2020-04-29 11:22:31 -0300

    Iniciando e configurando Cart_Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:restaurante/tabs/home_tab.dart';
 import 'package:restaurante/tabs/products_tab.dart';
+import 'package:restaurante/widgets/CartButton.dart';
 import 'package:restaurante/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
-  final pageController = PageController();
+  final _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return PageView(
-      controller: pageController,
+      controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
         Scaffold(
           body: HomeTab(),
-          drawer: CustomDrawer(pageController),
+          drawer: CustomDrawer(_pageController),
+          floatingActionButton: CartButton(),
         ),
         Scaffold(
           appBar: AppBar(
             title: Text("Categorias"),
             centerTitle: true,
           ),
-          drawer: CustomDrawer(pageController),
+          drawer: CustomDrawer(_pageController),
           body: ProductsTab(),
+          floatingActionButton: CartButton(),
         ),
         Container(
           color: Colors.yellow,
